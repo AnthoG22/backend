d2f0fafbd884314c33a73ff451ec5b5d2cc5a2aa
AnthoG22 <gonzalesanthony22@gmail.com>
2023-04-24 00:07:34 -0500

    prueba cors6

--- backend/models/tareaModels.js
+++ backend/models/tareaModels.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose')
 
 const tareaSchema = mongoose.Schema({
-    user:{
-        type : mongoose.Schema.Types.ObjectId,
-        ref : 'User',
-        required : true 
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
     },
-    texto:{
-        type : String,
-        required : [true , 'Por favor teclea una cadena']
+    texto: {
+        type: String,
+        required: [true, 'Por favor teclea una tarea']
     }
-},{
-    timestamp : true
-}
-)
-module.exports = mongoose.model('Tarea',tareaSchema)
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('Tarea', tareaSchema)
